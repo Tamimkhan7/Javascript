55c1e4d4a7ae904b15b7bd696321ecd2d73d9dc0
Tamim khan <mdtamimkhan842@gmail.com>
2024-04-22 01:38:02 +0600

    modified

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // alert("Hi Javascript");
-console.log("Hello world");
-document.getElementById('title').innerHTML = 'Hello world';
+console.log('Hello');
+document.getElementById('title').innerHTML = 'Hello world';//ami amar console ke bolci amar id ta doro then id ta ke update kore dew
+
+let sum = 50 + 40;//let is datatype
+console.log(sum);
+if (sum > 100) {
+    console.log('Expensive');
+} else {
+    console.log('cheep');
+}
+function sayHello(name) {
+    console
+}
