3adc9d1c1bed6349d79056097f73ea4253ec28cf
Tamim khan <mdtamimkhan842@gmail.com>
2024-06-08 00:34:55 +0600

    added varibale and modified index

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // alert("Hi Javascript");
 console.log('Hello');
 document.getElementById('title').innerHTML = 'Hello world';//ami amar console ke bolci amar id ta doro then id ta ke update kore dew
-
+// document.getElementById('title').innerHTML = 'Hello world';
 let sum = 50 + 40;//let is datatype
 console.log(sum);
 if (sum > 100) {
@@ -10,5 +10,14 @@ if (sum > 100) {
     console.log('cheep');
 }
 function sayHello(name) {
-    console
+    console.log(`${name}  vai cool `);
 }
+sayHello('Mahamud');
+
+
+
+let firstname = 'Tamimkhan';
+let age = 30;
+firstname = "Hannan khan";
+console.log(age);
+console.log(firstname);
---
+++ variable.js
@@ -0,0 +1,2 @@
+let furits = "Mango";
+console.log(`${furits} is my favourite food and i like it, ${furits} is the best for eating`);
