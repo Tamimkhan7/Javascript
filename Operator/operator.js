cd5bd12569a3501d77cce95609f33f384f4474c9
Tamim khan <mdtamimkhan842@gmail.com>
2024-12-22 01:36:06 +0600

    added operator

---
+++ Operator/operator.js
@@ -0,0 +1,28 @@
+// let userinput = prompt('Enter a number : ');
+// let number = parseInt(userinput);
+// if (number % 2 === 0) console.log(`${number} is even`);
+// else console.log(`${number} is odd`);
+
+
+
+//double quatation print between single quatation
+
+let name = "my name is 'tamim' khan";
+console.log(name);
+
+let fullname = 'my name is "tamim" khan';
+console.log(fullname);
+
+
+// which type is this datatype check it,, just use typeof function
+console.log(typeof fullname);
+
+
+let bignum = 999999999999999999;
+let bignum2 = 999999999999999999;
+console.log(bignum * bignum2);//big somossa holo aita scientific format print dicce normar format print korar jonno basically number a shesh a n added kore dite hopbe
+
+let numbig = 999999999999999999n;
+let numbig2 = 99999999999999999999n;
+console.log(numbig * numbig2);
+
