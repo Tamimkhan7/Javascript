b8860236fdf88c03421ea1f3bdc02cf86834c682
Tamim khan <mdtamimkhan842@gmail.com>
2025-01-13 01:55:53 +0600

    modified movie mania Project

--- Movie Mania Project/app.js
+++ Movie Mania Project/app.js
@@ -1,2 +1,55 @@
 import { getmoviereviewdata } from "./data.js";
-console.log(getmoviereviewdata());
+// console.log(getmoviereviewdata());
+function init() {
+    const moviereviesdata = getmoviereviewdata();
+    paintstatistics(moviereviesdata);
+    paintmoviedata(moviereviesdata);
+}
+
+function paintstatistics(moviereviesdata) {
+    const flatreviewdata = moviereviesdata.flat();
+
+    const totalmovies = moviereviesdata.length;
+    const totalreviews = flatreviewdata.length;
+    const totalrating = flatreviewdata.reduce((acc, item) => {
+        return acc + item.rating;
+    }, 0);
+    const averagerating = (totalrating / totalreviews).toFixed(2);
+
+    const totalmoviesE1 = document.getElementById("tmoviesId");
+    addstat(totalmoviesE1, totalmovies);
+
+    const averageratingEL = document.getElementById("taverageid");
+    addstat(averageratingEL, averagerating);
+
+    const totalreviewsEL = document.getElementById("treviewid");
+    addstat(totalreviewsEL, totalreviews);
+}
+
+function addstat(elem, value) {
+    const spanEL = document.createElement("span");
+    spanEL.classList.add("text-6xl");
+    spanEL.innerText = value;
+    elem.appendChild(spanEL);
+}
+
+function paintmoviedata(moviereviesdata) {
+    const flatreviewdata = moviereviesdata.flat();
+    const movielistEl = document.querySelector("#Movielistid UL"); //aite diye ami amar ul ta dorlam
+
+    // console.log(movielistEl);
+
+    flatreviewdata.map((movie) => {
+        const liElem = document.createElement("li");
+        liElem.classList.add("card", "my - 2", "p - 2");
+
+        const titleElem = document.createElement("p");
+        titleElem.classList.add("text-xl", "mb-2");
+        titleElem.innerText = `${movie.title} - ${movie.rating}`;
+        liElem.appendChild(titleElem);
+        movielistEl.appendChild(liElem);
+    })
+
+
+}
+init();
